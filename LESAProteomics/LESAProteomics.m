6f6f6e07401473c76e28b10abc85a3640c4fd2c3
jorismeurs <jorismeurs@gmail.com>
2020-06-23 17:02:58 +0200

    Redundant function removed

--- LESAProteomics/LESAProteomics.m
+++ LESAProteomics/LESAProteomics.m
@@ -1,7 +1,7 @@
 classdef LESAProteomics < visualise & identify & annotate & quantify
     
     properties (Constant = true)
-        version = '0.4.0';
+        version = '0.4.1';
         developer = 'Joris Meurs, MSc';
     end
     
--- LESAProteomics/annotation/writeMZXMLName.m
+++
@@ -1,6 +0,0 @@
-function obj = writeMZXMLName(obj)
-
-
-
-end
-
