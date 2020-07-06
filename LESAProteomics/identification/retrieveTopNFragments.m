1adb6f47586d37a8b8611e7762df7bdd09943bf3
jorismeurs <jorismeurs@gmail.com>
2020-07-06 10:03:43 +0200

    Create retrieveTopNFragments.m

---
+++ LESAProteomics/identification/retrieveTopNFragments.m
@@ -0,0 +1,41 @@
+function topN = retrieveTopNFragments(obj,spectrum,b,y)
+%
+% Most intense b and y ions
+
+YIons = [y.mz]';
+BIons = [b.mz]';
+
+intensities = [];
+for j = 1:length(YIons)
+    matchIon = find(spectrum(:,1) > YIons(j)-obj.settings.MS2Tolerance & ...
+        spectrum(:,1) < YIons(j)+obj.settings.MS2Tolerance);
+    if ~isempty(matchIon)
+       if numel(matchIon) > 1
+          diff = abs(YIons(j)-spectrum(matchIon,1));
+          min_diff = find(diff==min(diff));
+          intensities = [intensities;spectrum(matchIon(min_diff),2)];
+       else
+          intensities = [intensities;spectrum(matchIon,2)];
+       end
+    end
+end
+
+for j = 1:length(BIons)
+    matchIon = find(spectrum(:,1) > BIons(j)-obj.settings.MS2Tolerance & ...
+        spectrum(:,1) < BIons(j)+obj.settings.MS2Tolerance);
+    if ~isempty(matchIon)
+       if numel(matchIon) > 1
+          diff = abs(BIons(j)-spectrum(matchIon,1));
+          min_diff = find(diff==min(diff));
+          intensities = [intensities;spectrum(matchIon(min_diff),2)];  
+       else
+          intensities = [intensities;spectrum(matchIon,2)];
+       end
+    end
+end
+
+intensities = sort(intensities,'descend');
+topN = intensities(1:obj.settings.topNFragments,1);
+
+end
+
